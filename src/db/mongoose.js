2e75af59daf92e7e1a1ab5961eb77af11657d3c3
QwertyAswal <aswals8991@gmail.com>
2021-04-15 20:24:45 +0530

    Create endpoint created

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,77 +1,9 @@
 const mongoose = require('mongoose')
-const validator = require('validator')
 
 const connectionURL = 'mongodb://127.0.0.1:27017/task-manager-api'
 
-mongoose.connect(connectionURL, { useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true })
-
-const User = mongoose.model('User', {
-    name: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    email: {
-        type: String,
-        required: true,
-        trim: true,
-        lowercase: true,
-        validate(value) {
-            if (!validator.isEmail(value))
-                throw new Error('email invalid')
-        }
-    },
-    age: {
-        type: Number,
-        default: 0,
-        validate(value) {
-            if (value < 0) {
-                throw new Error('Age must be positive')
-            }
-        }
-    },
-    password: {
-        type: String,
-        required: true,
-        trim: true,
-        minLength: 7,
-        validate(value) {
-            if (value.toLowerCase().includes('password'))
-                throw new Error('Password can\'t contain the text \'password\'')
-        }
-    }
-})
-
-// const me = new User({
-//     name: '      Muskan          ',
-//     email: 'muskan@mail.COm',
-//     password:'qwertyassword         '
-// })
-
-// me.save().then((me) => {
-//     console.log(me)
-// }).catch((error) => {
-//     console.log('Error', error)
-// })
-
-const Task = mongoose.model('Task', {
-    description: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    completed: {
-        type: Boolean,
-        default: false
-    }
-})
-
-const task1 = new Task({
-    description: 'Assemble your pc               '
-})
-
-task1.save().then((task1) => {
-    console.log(task1)
-}).catch((error) => {
-    console.log(error)
+mongoose.connect(connectionURL, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useCreateIndex: true
 })
---
+++ src/models/user.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose')
+const validator = require('validator')
+
+const User = mongoose.model('User', {
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        lowercase: true,
+        validate(value) {
+            if (!validator.isEmail(value))
+                throw new Error('email invalid')
+        }
+    },
+    age: {
+        type: Number,
+        default: 0,
+        validate(value) {
+            if (value < 0) {
+                throw new Error('Age must be positive')
+            }
+        }
+    },
+    password: {
+        type: String,
+        required: true,
+        trim: true,
+        minLength: 7,
+        validate(value) {
+            if (value.toLowerCase().includes('password'))
+                throw new Error('Password can\'t contain the text \'password\'')
+        }
+    }
+})
+
+module.exports = User
