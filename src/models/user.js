e295e2930fe9cc7efc6a29e1825cf8b40fd3f20d
QwertyAswal <aswals8991@gmail.com>
2021-04-18 21:43:59 +0530

    corrected jwt error

--- src/models/user.js
+++ src/models/user.js
@@ -60,7 +60,7 @@ userSchema.virtual('tasks', {
 })
 
 userSchema.methods.generateAuthToken = async function () {
-    const token = await jwt.sign({ _id: this._id.toString() }, process.env.JWT_URL)
+    const token = await jwt.sign({ _id: this._id.toString() }, process.env.JWT_SECRET)
     this.tokens = this.tokens.concat({ token })
     await this.save()
     return token
